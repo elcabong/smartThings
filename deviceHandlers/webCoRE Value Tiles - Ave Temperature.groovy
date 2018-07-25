c12a5136b984dfb5c0b7dd00acb1aa23bc3a4d54
mbarone-laptop\mbarone <mbarone000@gmail.com>
2018-07-25 10:22:29 -0700

    updates tile view to show more information

--- deviceHandlers/webCoRE Value Tiles - Ave Temperature.groovy
+++ deviceHandlers/webCoRE Value Tiles - Ave Temperature.groovy
@@ -47,8 +47,8 @@ metadata {
     command "On"
     command "Off"
     }
- 	tiles {
-    	valueTile("Main", "device.Main", width: 2, height: 2) {
+ 	tiles(scale: 2){
+    	valueTile("Main", "device.Main", width: 6, height: 4) {
     		state("default", label:'${currentValue}°', icon: "st.alarm.temperature.normal",
        			backgroundColors:[
             		[value: 31, color: "#153591"],
@@ -60,14 +60,17 @@ metadata {
             		[value: 96, color: "#bc2323"]
         		]
     		)
-		}
-        standardTile("Refresh", "device.switch", inactiveLabel: false, width: 1, height: 1, decoration: "flat", wordWrap: true) {
-            state "off", action:"On", label: "", icon:"st.secondary.refresh"
-            state "on", label: "", icon:"st.motion.motion.active"
+		}		
+        standardTile("Refresh", "device.switch", inactiveLabel: false, width: 2, height: 1, decoration: "flat", wordWrap: true) {
+            state "off", action:"On", label: "Refresh", icon:"st.secondary.refresh"
+            state "on", label: "Refreshing", icon:"st.motion.motion.active"
         }
-        standardTile("Details", "device.Details", inactiveLabel: false, width: 1, height: 1, decoration: "flat", wordWrap: true) {
+        standardTile("Details", "device.Details", inactiveLabel: false, width: 4, height: 1, decoration: "flat", wordWrap: true) {
             state "default", label:'${currentValue}'
-        }
+        }		
+        valueTile("image", "image", width: 1, height: 1) {
+ 			state("default", label:'', icon: "st.alarm.temperature.normal")
+ 		}
         valueTile("Value0", "device.Value0", width: 1, height: 1) {
  			state( "default", label:'${currentValue}', unit:"dF",
        			backgroundColors:[
@@ -198,38 +201,38 @@ metadata {
         		]
     		)
  		}
-        valueTile("Title0", "device.Title0", width: 2, height: 1) {
+        valueTile("Title0", "device.Title0", width: 4, height: 1) {
  			state( "default", label:'${currentValue}')
  		}
-        valueTile("Title1", "device.Title1", width: 2, height: 1) {
+        valueTile("Title1", "device.Title1", width: 4, height: 1) {
  			state( "default", label:'${currentValue}')
  		}
-        valueTile("Title2", "device.Title2", width: 2, height: 1) {
+        valueTile("Title2", "device.Title2", width: 4, height: 1) {
  			state( "default", label:'${currentValue}')
  		}
-        valueTile("Title3", "device.Title3", width: 2, height: 1) {
+        valueTile("Title3", "device.Title3", width: 4, height: 1) {
  			state( "default", label:'${currentValue}')
  		}
-        valueTile("Title4", "device.Title4", width: 2, height: 1) {
+        valueTile("Title4", "device.Title4", width: 4, height: 1) {
  			state( "default", label:'${currentValue}')
  		}
-        valueTile("Title5", "device.Title5", width: 2, height: 1) {
+        valueTile("Title5", "device.Title5", width: 4, height: 1) {
  			state( "default", label:'${currentValue}')
  		}
-        valueTile("Title6", "device.Title6", width: 2, height: 1) {
+        valueTile("Title6", "device.Title6", width: 4, height: 1) {
  			state( "default", label:'${currentValue}')
  		}
-        valueTile("Title7", "device.Title7", width: 2, height: 1) {
+        valueTile("Title7", "device.Title7", width: 4, height: 1) {
  			state( "default", label:'${currentValue}')
  		}
-        valueTile("Title8", "device.Title8", width: 2, height: 1) {
+        valueTile("Title8", "device.Title8", width: 4, height: 1) {
  			state( "default", label:'${currentValue}')
  		}
-        valueTile("Title9", "device.Title9", width: 2, height: 1) {
+        valueTile("Title9", "device.Title9", width: 4, height: 1) {
  			state( "default", label:'${currentValue}')
  		}
  		main(["Main"])
- 		details(["Main","Refresh","Details","Title0","Value0","Title1","Value1","Title2","Value2","Title3","Value3","Title4","Value4","Title5","Value5","Title6","Value6","Title7","Value7","Title8","Value8","Title9","Value9"])
+ 		details(["Main","Refresh","Details","Title0","image","Value0","Title1","image","Value1","Title2","image","Value2","Title3","image","Value3","Title4","image","Value4","Title5","image","Value5","Title6","image","Value6","Title7","image","Value7","Title8","image","Value8","Title9","image","Value9"])
  	}
  }
  def parse(String description){
