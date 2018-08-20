f8bc79b5e5a2be8ea648de51c886a288751d40d5
mbarone-laptop\mbarone <mbarone000@gmail.com>
2018-08-20 09:14:42 -0700

    ave temp parent child initial commit

---
+++ deviceHandlers/webCoRE Value Tiles - Ave Temperature Group Child.groovy
@@ -0,0 +1,76 @@
+/**
+ *  webCoRE Value Ties - Ave Temperature Group Child
+ *
+ *  Copyright 2017 Daniel Ogorchock
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Change History:
+ *
+ *    Date        Who            What
+ *    ----        ---            ----
+ *    2017-04-10  Dan Ogorchock  Original Creation
+ *    2017-08-23  Allan (vseven) Added a generateEvent routine that gets info from the parent device.  This routine runs each time the value is updated which can lead to other modifications of the device.
+ *    2018-06-02  Dan Ogorchock  Revised/Simplified for Hubitat Composite Driver Model
+ *
+ * 
+ */
+metadata {
+	definition (name: "webCoRE Value Tiles - Ave Temperature Group Child", namespace: "mbarone/apps", author: "mbarone", vid:"generic-motion") {
+		capability "Temperature Measurement"
+		capability "Sensor"
+
+		attribute "lastUpdated", "String"
+	}
+
+	tiles(scale: 2) {
+		multiAttributeTile(name:"temperature", type: "generic"){
+			tileAttribute ("temperature", key: "PRIMARY_CONTROL") {
+				state("default", label:'${currentValue}°', icon: "st.alarm.temperature.normal",
+					backgroundColors:[
+						[value: 31, color: "#153591"],
+						[value: 44, color: "#1e9cbb"],
+						[value: 59, color: "#90d2a7"],
+						[value: 74, color: "#44b621"],
+						[value: 84, color: "#f1d801"],
+						[value: 95, color: "#d04e00"],
+						[value: 96, color: "#bc2323"]
+					]
+				)
+            }
+ 			tileAttribute("device.lastUpdated", key: "SECONDARY_CONTROL") {
+    				attributeState("default", label:'    Last updated ${currentValue}',icon: "st.Health & Wellness.health9")
+            }
+        }
+	}
+
+}
+
+def parse(String description) {
+    //log.trace "parse(${description}) called"
+	def parts = description.split(" ")
+    def name  = parts.length>0?parts[0].trim():null
+    def value = parts.length>1?parts[1].trim():null
+    if (name && value) {
+    	log.trace "name: "+name+", value:"+value
+        // Update device
+        sendEvent(name: name, value: value)
+        // Update lastUpdated date and time
+        def nowDay = new Date().format("MMM dd", location.timeZone)
+        def nowTime = new Date().format("h:mm a", location.timeZone)
+        sendEvent(name: "lastUpdated", value: nowDay + " at " + nowTime, displayed: false)
+    }
+    else {
+    	log.debug "Missing either name or value.  Cannot parse!"
+    }
+}
+
+def installed() {
+}
---
+++ deviceHandlers/webCoRE Value Tiles - Ave Temperature Group Master.groovy
@@ -0,0 +1,159 @@
+/**
+ *  Copyright 2018 mbarone
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+ 	definition (name: "webCoRE Value Ties - Ave Temperature Group Master", namespace: "mbarone/apps", author: "mbarone", vid:"generic-motion") {
+		capability "Temperature Measurement"
+		capability "Sensor"
+		capability "Switch"
+		capability "Health Check"
+		
+		attribute "Details","string"
+		
+		command "changeMain"
+		command "changeDetails"
+		command "changeChildValue"
+		command "clearValues"
+		command "open"
+		command "close"
+		command "On"
+		command "Off"
+		command "removeChildren"
+    }
+	
+ 	tiles(scale: 2){
+		multiAttributeTile(name:"Main", type: "generic", width: 6, height: 4) {
+			tileAttribute ("temperature", key: "PRIMARY_CONTROL") {
+				state("default", label:'${currentValue}°', icon: "st.alarm.temperature.normal",
+					backgroundColors:[
+						[value: 31, color: "#153591"],
+						[value: 44, color: "#1e9cbb"],
+						[value: 59, color: "#90d2a7"],
+						[value: 74, color: "#44b621"],
+						[value: 84, color: "#f1d801"],
+						[value: 95, color: "#d04e00"],
+						[value: 96, color: "#bc2323"]
+					]
+				)
+			}
+		}
+        standardTile("Refresh", "device.switch", inactiveLabel: false, width: 2, height: 1, decoration: "flat", wordWrap: true) {
+            state "off", action:"On", label: "Refresh", icon:"st.secondary.refresh"
+            state "on", action:"Off", label: "Refreshing", icon:"st.motion.motion.active"
+        }
+        standardTile("Details", "device.Details", inactiveLabel: false, width: 4, height: 1, decoration: "flat", wordWrap: true) {
+            state "default", label:'${currentValue}'
+        }
+		standardTile("removeChildren", "device.removeChildren", inactiveLabel: false, width: 6, height: 1, decoration: "flat", wordWrap: true){
+			state "default", label:'Remove Children', action:"removeChildren"
+		}
+		childDeviceTiles("All")
+		main(["Main"])
+		details(["Main","Refresh","Details","All","removeChildren"])
+	}
+ }
+ def parse(String description){
+ 	def pair = description.split(":")
+    createEvent(name: pair[0].trim(), value: pair[1].trim(), unit:"F")
+ }
+ def changeMain (param, details){
+	sendEvent("name":"temperature", "value": param)
+	sendEvent("name":"Details", "value":details)
+ }
+ def changeChildValue (title, param) {
+	def childDevice = null
+	def name = title
+	def value = param
+	def deviceType = "temperature"
+	try {
+		childDevices.each {
+			try{
+				//log.debug "1-Looking for child with deviceNetworkID = ${device.deviceNetworkId}-${name} against ${it.deviceNetworkId}"
+				if (it.deviceNetworkId == "${device.deviceNetworkId}-${name}") {
+					childDevice = it
+					//log.debug "Found a match 1!!!"
+				}
+			}
+			catch (e) {
+				log.debug e
+			}
+		}	
+		if (childDevice == null) {
+			//log.debug "isChild = true, but no child found - Auto Add it!"
+			//log.debug "    Need a ${name}"
+		
+			createChildDevice(name)
+			//find child again, since it should now exist!
+			childDevices.each {
+				try{
+					//log.debug "2-Looking for child with deviceNetworkID = ${device.deviceNetworkId}-${name} against ${it.deviceNetworkId}"
+					if (it.deviceNetworkId == "${device.deviceNetworkId}-${name}") {
+						childDevice = it
+						//log.debug "Found a match 2!!!"
+					}
+				}
+				catch (e) {
+					log.debug e
+				}
+			}
+		}
+		if (childDevice != null) {
+            //log.debug "parse() found child device ${childDevice.deviceNetworkId}"
+			//log.debug "sending parse(${deviceType} ${value})"
+            childDevice.parse("${deviceType} ${value}")
+			//log.debug "${childDevice.deviceNetworkId} - name: ${name} (contact), value: ${value}"
+		}
+	}
+	catch (e) {
+        log.error "Error in parse() routine, error = ${e}"
+	}
+ }
+ private void createChildDevice(String deviceName) {
+	log.trace "createChildDevice:  Creating Child Device '${device.displayName} (${deviceName})'"
+	try {
+		def deviceHandlerName = "webCoRE Value Ties - Contact Sensor Group Child"
+		addChildDevice(deviceHandlerName,
+						"${device.deviceNetworkId}-${deviceName}",
+						null,
+						[
+							completedSetup: true, 
+							label: "${device.displayName} (${deviceName})", 
+							isComponent: true, 
+							componentName: "${deviceName}", 
+							componentLabel: "${deviceName}"
+						]
+					)	
+	}
+	catch (e) {
+        log.error "Child device creation failed with error = ${e}"
+        state.alertMessage = "Child device creation failed. Please make sure that the '${deviceHandlerName}' is installed and published."
+	}
+ }
+ def removeChildren(){
+	log.trace "removing any child devices"
+	childDevices.each {
+		try{
+			deleteChildDevice(it.deviceNetworkId)
+		}
+		catch (e) {
+			log.debug "Error deleting ${it.deviceNetworkId}: ${e}"
+		}
+	}
+	sendEvent("name":"Details", "value":"Child devices removed!  May take some time to update below.  Refresh when ready to re-build child devices, or wait until a device changes state.")
+ }
+ def On(){
+ 	sendEvent(name: "switch", value: "on")
+ }
+ def Off(){
+ 	sendEvent(name: "switch", value: "off")
+ }
