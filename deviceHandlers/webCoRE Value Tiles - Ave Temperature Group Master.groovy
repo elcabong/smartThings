5bad4b4d4305c8a7a0d9ec0f5771baa621ceac52
mbarone-laptop\mbarone <mbarone000@gmail.com>
2018-08-20 17:24:06 -0700

    added webcore piston import code to headers and also url for smartthings forum post

--- deviceHandlers/webCoRE Value Tiles - Ave Temperature Group Master.groovy
+++ deviceHandlers/webCoRE Value Tiles - Ave Temperature Group Master.groovy
@@ -10,6 +10,20 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ *
+ *
+ *
+ *
+ *	To be used with the following webCoRE pistons:
+ *	 import code  -    piston name
+ *      sdbe   - "Average Temperatures"
+ *		j97uv  - "Average Temperatures ctrl"
+ *
+ *	See discussion thread:
+ *		https://community.smartthings.com/t/release-custom-dth-and-webcore-pistons-for-grouping-like-sensors-and-giving-1-aggregated-status-for-the-group/134270
+ *
+ *
+ *
  */
 metadata {
  	definition (name: "webCoRE Value Ties - Ave Temperature Group Master", namespace: "mbarone/apps", author: "mbarone", vid:"generic-motion") {
@@ -28,10 +42,9 @@ metadata {
     }
 	
  	tiles(scale: 2){
-		/*
 		multiAttributeTile(name:"Main", type: "generic", width: 6, height: 4) {
 			tileAttribute ("temperature", key: "PRIMARY_CONTROL") {
-				state("temperature", label:'${currentValue}°', icon: "st.alarm.temperature.normal",
+				attributeState("temperature", label:'${currentValue}°'
 					backgroundColors:[
 						[value: 31, color: "#153591"],
 						[value: 44, color: "#1e9cbb"],
@@ -43,20 +56,7 @@ metadata {
 					]
 				)
 			}
-		}*/
-        standardTile("Main", "temperature", inactiveLabel: false, width: 6, height: 4, decoration: "flat", wordWrap: true) {
-            state("temperature", label:'${currentValue}°',
-				backgroundColors:[
-					[value: 31, color: "#153591"],
-					[value: 44, color: "#1e9cbb"],
-					[value: 59, color: "#90d2a7"],
-					[value: 74, color: "#44b621"],
-					[value: 84, color: "#f1d801"],
-					[value: 95, color: "#d04e00"],
-					[value: 96, color: "#bc2323"]
-				]
-			)
-        }		
+		}		
         standardTile("Refresh", "device.switch", inactiveLabel: false, width: 2, height: 1, decoration: "flat", wordWrap: true) {
             state "off", action:"On", label: "Refresh", icon:"st.secondary.refresh"
             state "on", action:"Off", label: "Refreshing", icon:"st.motion.motion.active"
@@ -88,10 +88,10 @@ metadata {
 	try {
 		childDevices.each {
 			try{
-				log.debug "1-Looking for child with deviceNetworkID = ${device.deviceNetworkId}-${name} against ${it.deviceNetworkId}"
+				//log.debug "1-Looking for child with deviceNetworkID = ${device.deviceNetworkId}-${name} against ${it.deviceNetworkId}"
 				if (it.deviceNetworkId == "${device.deviceNetworkId}-${name}") {
 					childDevice = it
-					log.debug "Found a match 1!!!"
+					//log.debug "Found a match 1!!!"
 				}
 			}
 			catch (e) {
@@ -99,17 +99,17 @@ metadata {
 			}
 		}	
 		if (childDevice == null) {
-			log.debug "isChild = true, but no child found - Auto Add it!"
-			log.debug "    Need a ${name}"
+			//log.debug "isChild = true, but no child found - Auto Add it!"
+			//log.debug "    Need a ${name}"
 		
 			createChildDevice(name)
 			//find child again, since it should now exist!
 			childDevices.each {
 				try{
-					log.debug "2-Looking for child with deviceNetworkID = ${device.deviceNetworkId}-${name} against ${it.deviceNetworkId}"
+					//log.debug "2-Looking for child with deviceNetworkID = ${device.deviceNetworkId}-${name} against ${it.deviceNetworkId}"
 					if (it.deviceNetworkId == "${device.deviceNetworkId}-${name}") {
 						childDevice = it
-						log.debug "Found a match 2!!!"
+						//log.debug "Found a match 2!!!"
 					}
 				}
 				catch (e) {
@@ -118,8 +118,8 @@ metadata {
 			}
 		}
 		if (childDevice != null) {
-            log.debug "parse() found child device ${childDevice.deviceNetworkId}"
-			log.debug "sending parse(${deviceType} ${value})"
+            //log.debug "parse() found child device ${childDevice.deviceNetworkId}"
+			//log.debug "sending parse(${deviceType} ${value})"
             childDevice.parse("${deviceType} ${value}")
 			log.debug "${childDevice.deviceNetworkId} - name: ${name} (temperature), value: ${value}"
 		}
@@ -150,7 +150,7 @@ metadata {
 	}
  }
  def removeChildren(){
-	log.trace "removing any child devices"
+	log.trace "starting: removing any child devices"
 	childDevices.each {
 		try{
 			log.trace "removing ${it.deviceNetworkId}"
--- deviceHandlers/webCoRE Value Tiles - Contact Sensor Group Master.groovy
+++ deviceHandlers/webCoRE Value Tiles - Contact Sensor Group Master.groovy
@@ -10,6 +10,17 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ *
+ *
+ *	To be used with the following webCoRE piston:
+ *	 import code  -    piston name
+ *      50bko   - "All Exterior Doors"
+ *
+ *	See discussion thread:
+ *		https://community.smartthings.com/t/release-custom-dth-and-webcore-pistons-for-grouping-like-sensors-and-giving-1-aggregated-status-for-the-group/134270
+ *
+ *
+ *
  */
 metadata {
  	definition (name: "webCoRE Value Ties - Contact Sensor Group Master", namespace: "mbarone/apps", author: "mbarone", vid:"generic-contact") {
@@ -106,7 +117,7 @@ metadata {
             //log.debug "parse() found child device ${childDevice.deviceNetworkId}"
 			//log.debug "sending parse(${deviceType} ${value})"
             childDevice.parse("${deviceType} ${value}")
-			//log.debug "${childDevice.deviceNetworkId} - name: ${name} (contact), value: ${value}"
+			log.debug "${childDevice.deviceNetworkId} - name: ${name} (contact), value: ${value}"
 		}
 	}
 	catch (e) {
@@ -146,6 +157,7 @@ metadata {
 	log.trace "removing any child devices"
 	childDevices.each {
 		try{
+			log.trace "removing ${it.deviceNetworkId}"
 			deleteChildDevice(it.deviceNetworkId)
 		}
 		catch (e) {
