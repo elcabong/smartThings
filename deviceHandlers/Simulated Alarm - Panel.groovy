990a89d3399f4398b932a68b72a153e421650402
mbarone-laptop\mbarone <mbarone000@gmail.com>
2018-08-24 15:20:53 -0700

    added webCoRE piston import code to dth

--- deviceHandlers/Simulated Alarm - Panel.groovy
+++ deviceHandlers/Simulated Alarm - Panel.groovy
@@ -15,6 +15,11 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ *
+ *
+ *	This device type handler requires webCoRE to function and keep in sync with SHM
+ *		piston import code:  xh6q
+ *
  */
 metadata {
 	definition (name: "Simulated Alarm - Panel", namespace: "mbarone/apps", author: "mbarone") {
