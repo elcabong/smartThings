5f5ead7a1f0ef03b4ddaa0cc7b0b5264461ef957
mbarone <mbarone000@gmail.com>
2019-09-28 14:20:12 -0700

    text fixed for button

--- devicetypes/mbarone/gocontrol-external-contact-sensor.src/gocontrol-external-contact-sensor.groovy
+++ devicetypes/mbarone/gocontrol-external-contact-sensor.src/gocontrol-external-contact-sensor.groovy
@@ -165,11 +165,11 @@ metadata {
 		}		
 
 		standardTile("forceOpen", "device.open", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-			state "default", label:'Force Open', action:"open", icon:"st.contact.contact.open", backgroundColor:"#00a0dc"
+			state "default", label:'Set Open', action:"open", icon:"st.contact.contact.open", backgroundColor:"#00a0dc"
 		}		
 
 		standardTile("forceClose", "device.close", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-			state "default", label:'Force Closed', action:"close", icon:"st.contact.contact.closed", backgroundColor:"#00a0dc"
+			state "default", label:'Set Closed', action:"close", icon:"st.contact.contact.closed", backgroundColor:"#00a0dc"
 		}
 		
 		main("mainTile")
